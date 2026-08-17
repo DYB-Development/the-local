c284ca6e510dc8e2d1d1e822f985c4f52b3c3ab6
tylercschneider <tylercschneider@gmail.com>
2026-08-17 16:08:00 -0400

    Stop re-exporting the companion agents

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ export {
   processRules,
   writeProcessDoc,
 } from "./process.js";
-export { companionAgents } from "./companion.js";
 export { creatorPrompt } from "./creators.js";
 export {
   INTERFACE_FILE,
